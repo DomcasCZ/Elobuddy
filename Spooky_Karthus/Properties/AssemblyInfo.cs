e178173e3acb7450225c83f5987adcff0cb2778d
wladi0 <lena.epvp@web.de>
2016-09-17 01:38:17 +0200

    Update AssemblyInfo.cs

--- Spooky_Karthus/Properties/AssemblyInfo.cs
+++ Spooky_Karthus/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Karthus")]
+[assembly: AssemblyProduct("Spooky_Karthus")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
