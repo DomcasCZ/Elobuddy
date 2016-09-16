a41dc9981271c7947f937c7a87255843a73f643f
wladi0 <lena.epvp@web.de>
2016-09-17 01:50:55 +0200

    Dark_Syndra

---
+++ Fairy_Lux/Active.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using T2IN1_Lib;
+using static Fairy_Lux.SpellsManager;
+
+namespace Fairy_Lux
+{
+    class Active
+    {
+        public static void Execute6()
+        {
+            if (W.IsReady())
+
+                if (myhero.IsInDanger(80))
+                {
+                    W.Cast(myhero.Position);
+                }
+
+        }
+
+        public static AIHeroClient myhero { get { return ObjectManager.Player; } }
+    }
+
+}
