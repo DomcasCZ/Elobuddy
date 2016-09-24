065dd5f651f299a88fd308042b09725c14f0a845
wladi0 <lena.epvp@web.de>
2016-09-24 18:49:48 +0200

    Update SpellsManager.cs

--- Fairy_Lux/SpellsManager.cs
+++ Fairy_Lux/SpellsManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using EloBuddy;
 using EloBuddy.SDK;
 using EloBuddy.SDK.Enumerations;
@@ -49,7 +49,7 @@ namespace Fairy_Lux
             {
                 case SpellSlot.Q:
                     if (Q.IsReady())
-                        dmg += new float[] { 50, 100, 150, 200, 250 }[sLevel] + 0.7f*ap;
+                        dmg += new float[] { 100, 200, 300, 400, 500 }[sLevel] + 0.7f*ap;
                     break;                  // 50, 100, 150, 200, 250
                 case SpellSlot.W:
                     if (W.IsReady())
@@ -57,11 +57,11 @@ namespace Fairy_Lux
                     break;                  
                 case SpellSlot.E:
                     if (E.IsReady())
-                        dmg += new float[] { 60, 105, 150, 195, 240 }[sLevel] + 0.6f*ap;
+                        dmg += new float[] { 120, 210, 300, 400, 480 }[sLevel] + 0.6f*ap;
                     break;                  //60, 105, 150, 195, 240
                 case SpellSlot.R:
                     if (R.IsReady())
-                        dmg += new float[] { 300, 400, 500 }[sLevel] + 0.75f*ap;
+                        dmg += new float[] { 600, 800, 1000 }[sLevel] + 0.75f*ap;
                     break;                  //300 400 500
             }
             return Player.Instance.CalculateDamageOnUnit(target, damageType, dmg - 10);
