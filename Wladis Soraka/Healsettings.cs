7f70489a6526b280cd94c4212d25fdc2391bafbe
wladi0 <lena.epvp@web.de>
2016-10-11 20:57:33 +0200

    Soraka
    Soraka

---
+++ Wladis Soraka/Healsettings.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+using static Wladis_Soraka.Combo;
+using static Wladis_Soraka.Menus;
+using System;
+
+namespace Wladis_Soraka
+{
+    class HealSettings
+    {
+        public static void Execute6()
+        {
+            var test = EntityManager.Heroes.Allies.Where(hero => !hero.IsMe && !hero.IsDead && !hero.IsInShopRange() && !hero.IsZombie && hero.Distance(myhero) <= SpellsManager.W.Range);
+
+            var sdl = EntityManager.Heroes.Allies.FirstOrDefault(hero => !hero.IsMe && !hero.IsInShopRange() && !hero.IsZombie && hero.Distance(myhero) <= SpellsManager.W.Range);
+
+            var target = TargetSelector.GetTarget(SpellsManager.Q.Range, DamageType.Magical);
+            
+            if (HealMenu["AutoW"].Cast<CheckBox>().CurrentValue && SpellsManager.W.IsReady() && myhero.HealthPercent > HealMenu["Myhealth"].Cast<Slider>().CurrentValue && sdl.HealthPercent < HealMenu["WAllyHealth"].Cast<Slider>().CurrentValue)
+            {
+                SpellsManager.W.Cast(sdl);
+            }
+        }
+
+        public static void Execute8()
+        {
+            var test = EntityManager.Heroes.Allies.Where(hero => !hero.IsMe && !hero.IsDead && !hero.IsInShopRange() && !hero.IsZombie && hero.Distance(myhero) <= SpellsManager.R.Range);
+            var allytoheal = test.FirstOrDefault(x => !x.IsInShopRange());
+            var sdl = EntityManager.Heroes.Allies.FirstOrDefault(hero => !hero.IsMe && !hero.IsInShopRange() && !hero.IsZombie && hero.Distance(myhero) <= SpellsManager.R.Range);
+
+            if (SpellsManager.R.IsReady() && HealMenu["R"].Cast<CheckBox>().CurrentValue && sdl.HealthPercent < HealMenu["RAllyHealth"].Cast<Slider>().CurrentValue && sdl.CountEnemiesInRange(HealMenu["REnemyInRange"].Cast<Slider>().CurrentValue) >= 1)
+            {
+                SpellsManager.R.Cast();
+            }
+            
+                if (SpellsManager.R.IsReady() && HealMenu["RYou"].Cast<CheckBox>().CurrentValue && myhero.HealthPercent < HealMenu["RAllyHealth"].Cast<Slider>().CurrentValue && myhero.CountEnemiesInRange(HealMenu["REnemyInRange"].Cast<Slider>().CurrentValue) >= 1)
+                {
+                    SpellsManager.R.Cast();
+                }
+        }
+        
+    }
+
+}
---
+++ Wladis Soraka/ModeManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+using static Wladis_Soraka.Menus;
+using EloBuddy.SDK.Events;
+
+namespace Wladis_Soraka
+{
+    internal class ModeManager
+    {
+        public static void InitializeModes()
+        {
+            Game.OnTick += Game_OnTick;
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            var orbMode = Orbwalker.ActiveModesFlags;
+            var playerMana = Player.Instance.ManaPercent;
+            var target = TargetSelector.GetTarget(SpellsManager.E.Range, DamageType.Magical);
+
+            if (orbMode.HasFlag(Orbwalker.ActiveModes.Combo))
+                Combo.Execute();
+
+            if (orbMode.HasFlag(Orbwalker.ActiveModes.Harass))
+                Combo.Execute1();
+
+            if (orbMode.HasFlag(Orbwalker.ActiveModes.Flee))
+                Combo.Execute3();
+
+            if (orbMode.HasFlag(Orbwalker.ActiveModes.LaneClear))
+                Combo.Execute4();
+
+            if (ComboMenu["AutoQ"].Cast<CheckBox>().CurrentValue)
+                Combo.Execute7();
+
+            if (HealMenu["AutoW"].Cast<CheckBox>().CurrentValue)
+                HealSettings.Execute6();
+
+            if (HealMenu["R"].Cast<CheckBox>().CurrentValue || HealMenu["RYou"].Cast<CheckBox>().CurrentValue)
+                HealSettings.Execute8();
+            
+            if (MiscMenu["EStun"].Cast<CheckBox>().CurrentValue)
+            if (target.IsCharmed || target.IsStunned || target.IsTaunted || target.IsRooted || target.IsFeared)
+            {
+                var pred = SpellsManager.E.GetPrediction(target);
+                SpellsManager.E.Cast(pred.CastPosition);
+            }
+        }
+
+        private static void Gapcloser_OnGapcloser(AIHeroClient sender, Gapcloser.GapcloserEventArgs e)
+        {
+            if (Menus.MiscMenu["Gapcloser"].Cast<CheckBox>().CurrentValue)
+                if (!sender.IsEnemy) return;
+
+            if (sender.IsValidTarget(SpellsManager.E.Range))
+            {
+                SpellsManager.E.Cast(sender.Position);
+            }
+        }
+    }
+}
